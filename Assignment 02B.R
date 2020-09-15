2ad49c83dbb6904d86848a1ed8262d8956973d8e
Sophie Robinson <scr224@lehigh.edu>
2020-09-15 09:57:36 -0400

    Trial 1

---
+++ Assignment 02B.R
@@ -0,0 +1,23 @@
+# Data Frames
+x <- 10:1
+y <- -4:5
+z <- c('Hockey', 'Football', 'Curling', 'Soccer', 'Rugby', 'Baseball', 'Golf', 'Basketball', 'Wrestling', 'Tennis')
+theDF <- data.frame(x,y,z)
+theDF
+
+str(theDF)
+
+theDF[1,2]
+View(theDF)
+
+# Using names
+theDF$x
+theDF$x[3]
+
+# Editing names
+names(theDF)<-c("Popularity", "Team Strength", "Sport")
+View(theDF)
+
+# Print sum of integers in Popularity
+sum(theDF$Popularity)
+
