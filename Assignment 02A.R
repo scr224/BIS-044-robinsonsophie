1344f94a5fc1ed9f5ac7f01a003e148c2de8b21c
Sophie Robinson <scr224@lehigh.edu>
2020-09-15 09:57:36 -0400

    Completed Assignment

---
+++ Assignment 02A.R
@@ -0,0 +1,5 @@
+#Difference in dates
+DaysLeft <- as.double(as.Date("2020-12-04") - Sys.Date())
+                      
+#Print days left in semester
+cat("Sophie, there are", DaysLeft, "days left in the semester")
